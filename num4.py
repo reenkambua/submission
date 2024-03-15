f73737264436d3d71df63d082f15431ebd574d95
reenkambua <126237874+reenkambua@users.noreply.github.com>
2024-03-15 23:52:01 +0300

    Add files via upload

---
+++ num4.py
@@ -0,0 +1,4 @@
+#Write a program that accepts a string as input, capitalizes the first letter of each word in the string, and then returns the result string.
+
+sentence=str(input("Enter a sentence: "))
+print(sentence.title())
